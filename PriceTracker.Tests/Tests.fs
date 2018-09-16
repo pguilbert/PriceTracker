dc1ddeb469132beff818811d013f7360108cc6b3
Paul Guilbert <guilbert.paul@gmail.com>
2018-09-16 19:38:04 +0200

    Tests update

--- PriceTracker.Tests/Tests.fs
+++ PriceTracker.Tests/Tests.fs
@@ -3,18 +3,27 @@
 open Xunit
 open PriceTracker
 
+let wrongDomainUrl = "http://google.com"
+let unableToExtractProductUrl = "https://www.amazon.fr/"
+let notFoundValidUrl = "https://www.amazon.fr/flmdjsfljfgdsf"
+let okUrl = "https://www.amazon.fr/gp/product/B0756CYWWD"
+
 [<Fact>]
 let ``When URL is wrong expect InvalidPriceProviderUrl``() =
-    Assert.Equal(Result.Error(Message.InvalidPriceProviderUrl), Amazon.getPrice "http://google.com")
+    Assert.Equal(Result.Error(Message.InvalidPriceProviderUrl), Amazon.getPrice wrongDomainUrl)
 
 [<Fact>]
 let ``When provider URL is ok but not a product page expect NoPriceFound``() =
-    Assert.Equal(Result.Error(Message.NoPriceFound), Amazon.getPrice "https://www.amazon.fr/")
+    Assert.Equal(Result.Error(Message.NoPriceFound), Amazon.getPrice unableToExtractProductUrl)
+
+[<Fact>]
+let ``When provider URL domain is ok but the page is not found expect UrlNotFoundOnTheProviderServer``() =
+    Assert.Equal(Result.Error(Message.UrlNotFoundOnTheProviderServer), Amazon.getPrice notFoundValidUrl)
 
 [<Fact>]
 let ``When URL is ok expect price``() =
     let test = 
-        Amazon.getPrice "https://www.amazon.fr/gp/product/B0756CYWWD" 
+        Amazon.getPrice okUrl 
         |> function
             | Result.Ok _ -> true
             | Result.Error _ -> false
